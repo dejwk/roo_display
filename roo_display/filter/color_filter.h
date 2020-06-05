c868dc1c1ce4685a6711dc71896122098fb29a7d
Dawid Kurzyniec <dawidk@google.com>
2020-06-05 21:21:38 +0200

    Added color_filter. It allows dynamic color remapping of drawn objects, allowing effects such as: added translucency (e.g. fade-in effect), modifying contrast/brightness/saturation, etc.

---
+++ roo_display/filter/color_filter.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "roo_display/core/color.h"
+#include "roo_display/core/device.h"
+
+namespace roo_display {
+
+template <typename Filter>
+class ColorFilter : public DisplayOutput {
+ public:
+  ColorFilter(DisplayOutput* output, Filter filter = Filter(), Color bgcolor = color::Transparent)
+      : output_(output),
+        filter_(filter),
+        bgcolor_(bgcolor) {}
+
+  ColorFilter(DisplayOutput* output, Color bgcolor = color::Transparent)
+      : ColorFilter(output, Filter(), bgcolor) {}
+
+  virtual ~ColorFilter() {}
+
+  void setAddress(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+                  PaintMode mode) override {
+    output_->setAddress(x0, y0, x1, y1, mode);
+  }
+
+  void write(Color* color, uint32_t pixel_count) override {
+    transform(color, pixel_count);
+    output_->write(color, pixel_count);
+  }
+
+  void writeRects(PaintMode mode, Color* color, int16_t* x0, int16_t* y0,
+                  int16_t* x1, int16_t* y1, uint16_t count) override {
+    transform(color, count);
+    output_->writeRects(mode, color, x0, y0, x1, y1, count);
+  }
+
+  void fillRects(PaintMode mode, Color color, int16_t* x0, int16_t* y0,
+                 int16_t* x1, int16_t* y1, uint16_t count) override {
+    output_->fillRects(mode, transform(color), x0, y0, x1, y1, count);
+  }
+
+  void writePixels(PaintMode mode, Color* color, int16_t* x, int16_t* y,
+                   uint16_t pixel_count) override {
+    transform(color, pixel_count);
+    output_->writePixels(mode, color, x, y, pixel_count);
+  }
+
+  void fillPixels(PaintMode mode, Color color, int16_t* x, int16_t* y,
+                  uint16_t pixel_count) override {
+    output_->fillPixels(mode, transform(color), x, y, pixel_count);
+  }
+
+ private:
+  void transform(Color* colors, uint16_t count) const {
+    for (uint16_t i = 0; i < count; i++) {
+      colors[i] = transform(colors[i]);
+    }
+  }
+
+  Color transform(Color c) const {
+    return filter_(c, bgcolor_);
+  }
+
+  DisplayOutput* output_;
+  Filter filter_;
+  Color bgcolor_;
+};
+
+class Opaqueness {
+ public:
+  Opaqueness(uint8_t opaqueness) : opaqueness_(opaqueness) {}
+
+  Color operator()(Color c, Color bg) const {
+    c.set_a((c.a() * opaqueness_) >> 7);
+    return alphaBlend(bg, c);
+  }
+
+ private:
+  uint8_t opaqueness_;
+};
+
+// A 'filtering' device that adds translucency (specified in the [0-128] range).
+typedef ColorFilter<Opaqueness> TranslucencyFilter;
+
+namespace internal {
+
+struct Disablement {
+  Color operator()(Color c, Color bg) const {
+    uint8_t a = (c.a() * 32) >> 7;
+    uint8_t gray = (((int16_t)c.r() * 3) + ((int16_t)c.g() * 4) + c.b()) >> 3;
+    return alphaBlend(bg, Color(a << 24 | gray * 0x010101));
+    //return color::Transparent;
+  }
+};
+
+}  // namespace
+
+typedef ColorFilter<internal::Disablement> DisablementFilter;
+
+}  // namespace roo_display

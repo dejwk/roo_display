14ef3ef9f10c1e473387c8097bd28f5dd45113be
Dawid Kurzyniec <dawidk@google.com>
2022-01-08 17:09:45 +0100

    Added the color gradient.

---
+++ roo_display/ui/color_gradient.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <initializer_list>
+#include <utility>
+#include <vector>
+
+#include "roo_display/core/color.h"
+
+namespace roo_display {
+
+// Given the list of colors at a handful of (at least two) specified
+// floating-point values, dynamically generates colors for arbitrary
+// floating-point values, using linear interpolation.
+class ColorGradient {
+ public:
+  // The gradient argument must be sorted by the first (double) element.
+  ColorGradient(
+      const std::initializer_list<std::pair<double, roo_display::Color>>&
+          gradient)
+      : gradient_(gradient) {}
+
+  roo_display::Color getColor(double value) const;
+
+ private:
+  std::vector<std::pair<double, roo_display::Color>> gradient_;
+};
+
+}  // namespace roo_display

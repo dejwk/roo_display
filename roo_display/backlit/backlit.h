aba79c8fdb4c7d0b34bbe7bcaba929682ce76e00
Dawid Kurzyniec <dawidk@google.com>
2023-03-25 18:46:26 +0100

    Adding basic support for backlit.

---
+++ roo_display/backlit/backlit.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace roo_display {
+
+class Backlit {
+ public:
+  virtual ~Backlit() = default;
+  // Sets backlit intensity in the range [0..255].
+  virtual void setIntensity(uint8_t intensity) = 0;
+};
+
+class NoBacklit : public Backlit {
+ public:
+  void setIntensity(uint8_t intensity) override {}
+};
+
+}  // namespace
---
+++ roo_display/backlit/esp32_ledc.h
@@ -0,0 +1,25 @@
+#include "esp32-hal.h"
+#include "roo_display/backlit/backlit.h"
+
+namespace roo_display {
+
+class LedcBacklit : public Backlit {
+ public:
+  LedcBacklit(int pin, int channel, uint8_t intensity = 255)
+      : pin_(pin), channel_(channel) {
+    pinMode(pin, OUTPUT);
+    ledcAttachPin(pin, channel);
+    ledcSetup(channel, 50000, 8);
+    ledcWrite(channel, intensity);
+  }
+
+  void setIntensity(uint8_t intensity) override {
+    ledcWrite(channel_, intensity);
+  }
+
+ private:
+  int pin_;
+  int channel_;
+};
+
+};  // namespace roo_display

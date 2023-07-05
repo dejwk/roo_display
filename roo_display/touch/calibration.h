3a508983afd0cc28ef42aac2781255f0d7675d2f
Dawid Kurzyniec <dawidk@google.com>
2023-07-05 22:03:57 +0200

    Making TouchCalibration constexpr, so that it can be used as a static constant.

--- roo_display/touch/calibration.h
+++ roo_display/touch/calibration.h
@@ -9,16 +9,17 @@ namespace roo_display {
 class TouchCalibration {
  public:
   // Effectively uncalibrated, put possibly reoriented.
-  TouchCalibration(Orientation orientation = Orientation::Default())
+  constexpr TouchCalibration(Orientation orientation = Orientation::Default())
       : TouchCalibration(Box(0, 0, 4095, 4095), orientation) {}
 
   // Calibrated and possibly reoriented.
-  TouchCalibration(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-                   Orientation orientation = Orientation::Default())
+  constexpr TouchCalibration(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+                             Orientation orientation = Orientation::Default())
       : TouchCalibration(Box(x0, y0, x1, y1), orientation) {}
 
   // Calibrated and possibly reoriented.
-  TouchCalibration(Box bounds, Orientation orientation = Orientation::Default())
+  constexpr TouchCalibration(Box bounds,
+                             Orientation orientation = Orientation::Default())
       : bounds_(std::move(bounds)), orientation_(orientation) {}
 
   // Augments the touch point according to the calibration spec.

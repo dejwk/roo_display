f186a83eb7049875b045ed27288ad013608cdc04
dejwk <dawid.kurzyniec@gmail.com>
2025-01-11 23:06:12 +0100

    Ported RGB panel code.

--- src/roo_display/driver/esp32s3_dma_parallel_rgb565.cpp
+++ src/roo_display/driver/esp32s3_dma_parallel_rgb565.cpp
@@ -7,60 +7,18 @@
 #include "esp_lcd_panel_interface.h"
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
-#include "esp_lcd_panel_rgb.h"
+// #include "esp_lcd_panel_rgb.h"
 #include "esp_pm.h"
 #include "esp_private/gdma.h"
 #include "hal/dma_types.h"
 #include "hal/lcd_hal.h"
 #include "roo_display/driver/esp32s3_dma_parallel_rgb565.h"
 
+#include "roo_display/driver/common/rgb_panel.h"
 
 namespace roo_display {
 
 namespace esp32s3_dma {
-
-// extract from esp-idf esp_lcd_rgb_panel.c
-struct esp_rgb_panel_t {
-  esp_lcd_panel_t base;   // Base class of generic lcd panel
-  int panel_id;           // LCD panel ID
-  lcd_hal_context_t hal;  // Hal layer object
-  size_t data_width;  // Number of data lines (e.g. for RGB565, the data width
-                      // is 16)
-  size_t sram_trans_align;  // Alignment for framebuffer that allocated in SRAM
-  size_t
-      psram_trans_align;  // Alignment for framebuffer that allocated in PSRAM
-  int disp_gpio_num;   // Display control GPIO, which is used to perform action
-                       // like "disp_off"
-  intr_handle_t intr;  // LCD peripheral interrupt handle
-  esp_pm_lock_handle_t pm_lock;  // Power management lock
-  size_t num_dma_nodes;  // Number of DMA descriptors that used to carry the
-                         // frame buffer
-  uint8_t *fb;           // Frame buffer
-  size_t fb_size;        // Size of frame buffer
-  int data_gpio_nums[SOC_LCD_RGB_DATA_WIDTH];  // GPIOs used for data lines, we
-                                               // keep these GPIOs for action
-                                               // like "invert_color"
-  size_t resolution_hz;                        // Peripheral clock resolution
-  esp_lcd_rgb_timing_t
-      timings;  // RGB timing parameters (e.g. pclk, sync pulse, porch width)
-  gdma_channel_handle_t dma_chan;  // DMA channel handle
-  esp_lcd_rgb_panel_frame_trans_done_cb_t
-      on_frame_trans_done;  // Callback, invoked after frame trans done
-  void *user_ctx;           // Reserved user's data of callback functions
-  int x_gap;  // Extra gap in x coordinate, it's used when calculate the flush
-              // window
-  int y_gap;  // Extra gap in y coordinate, it's used when calculate the flush
-              // window
-  struct {
-    unsigned int disp_en_level : 1;  // The level which can turn on the screen
-                                     // by `disp_gpio_num`
-    unsigned int stream_mode : 1;    // If set, the LCD transfers data
-                                     // continuously, otherwise, it stops
-                                     // refreshing the LCD when transaction done
-    unsigned int fb_in_psram : 1;    // Whether the frame buffer is in PSRAM
-  } flags;
-  dma_descriptor_t dma_nodes[];  // DMA descriptor pool of size `num_dma_nodes`
-};
 
 uint8_t *AllocateBuffer(const Config &config) {
   esp_lcd_rgb_panel_config_t *cfg =
@@ -148,8 +106,9 @@ uint8_t *AllocateBuffer(const Config &config) {
   ESP_ERROR_CHECK(esp_lcd_panel_reset(handle));
   ESP_ERROR_CHECK(esp_lcd_panel_init(handle));
 
-  esp_rgb_panel_t *rgb_panel = __containerof(handle, esp_rgb_panel_t, base);
-  return (uint8_t *)rgb_panel->fb;
+  uint8_t* buf;
+  esp_lcd_rgb_panel_get_frame_buffer(handle, 1, (void**)&buf);
+  return buf;
 }
 
 template <>

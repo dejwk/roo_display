7cdf4718318e8a8fbb7514df3fd4f99f1c97214e
dejwk <dawid.kurzyniec@gmail.com>
2025-01-15 18:27:28 +0100

    Fixed ESP32 backlit to work with Arduino version 3.

--- src/roo_display/backlit/esp32_ledc.h
+++ src/roo_display/backlit/esp32_ledc.h
@@ -7,14 +7,28 @@ namespace roo_display {
 
 class LedcBacklit : public Backlit {
  public:
+  LedcBacklit(int pin, uint8_t intensity = 255)
+      : LedcBacklit(pin, 0, intensity) {}
+
+#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
+ private:
+#else
+ public:
+#endif
   LedcBacklit(int pin, int channel, uint8_t intensity = 255)
       : pin_(pin), channel_(channel) {
     pinMode(pin, OUTPUT);
+
+#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
+    ledcAttach(pin_, 50000, 8);
+#else
     ledcSetup(channel_, 50000, 8);
     ledcAttachPin(pin_, channel_);
+#endif
     ledcWrite(channel_, intensity);
   }
 
+ public:
   void setIntensity(uint8_t intensity) override {
     ledcWrite(channel_, intensity);
   }

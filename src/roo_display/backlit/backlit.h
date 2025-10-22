aca522c535ab5df33e29a68a534e71857ca7c942
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-10-22 20:28:07 +0200

    Changes in the backlit interface:
    * Avoid pinMode() in the constructor; it doesn't work on some microcontrollers.
    * Updated the implementation for the Arduino version 3.

--- src/roo_display/backlit/backlit.h
+++ src/roo_display/backlit/backlit.h
@@ -6,14 +6,18 @@ namespace roo_display {
 
 class Backlit {
  public:
+  static constexpr uint16_t kMaxIntensity = 256;
+  static constexpr uint16_t kHalfIntensity = 128;
+
   virtual ~Backlit() = default;
-  // Sets backlit intensity in the range [0..255].
-  virtual void setIntensity(uint8_t intensity) = 0;
+
+  // Sets backlit intensity in the range [0..256].
+  virtual void setIntensity(uint16_t intensity) = 0;
 };
 
 class NoBacklit : public Backlit {
  public:
-  void setIntensity(uint8_t intensity) override {}
+  void setIntensity(uint16_t intensity) override {}
 };
 
-}  // namespace
+}  // namespace roo_display
--- src/roo_display/backlit/esp32_ledc.h
+++ src/roo_display/backlit/esp32_ledc.h
@@ -5,42 +5,77 @@
 
 namespace roo_display {
 
+#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
+
 class LedcBacklit : public Backlit {
  public:
-  LedcBacklit(int pin, uint8_t intensity = 255)
-      : LedcBacklit(pin, 0, intensity) {}
+  LedcBacklit(uint8_t pin) : pin_(pin) {}
 
-#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
- private:
-#else
  public:
-#endif
-  LedcBacklit(int pin, int channel, uint8_t intensity = 255)
-      : pin_(pin), channel_(channel) {
-    pinMode(pin, OUTPUT);
+  void begin(uint16_t intensity = kMaxIntensity) {
+    pinMode(pin_, OUTPUT);
+    // Use 50 kHz frequency to avoid interference with audio.
+    CHECK(ledcAttach(pin_, 50000, 8));
+    CHECK(ledcWrite(pin_, intensity));
+  }
 
-#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
-    ledcAttach(pin_, 50000, 8);
-    ledcWrite(pin_, intensity);
+  void setIntensity(uint16_t intensity) override {
+    CHECK(ledcWrite(pin_, intensity));
+  }
+
+ private:
+  uint8_t pin_;
+};
+
+class LedcBacklitAtChannel : public Backlit {
+ public:
+  LedcBacklitAtChannel(uint8_t pin, uint8_t channel)
+      : pin_(pin), channel_(channel) {}
+
+ public:
+  void begin(uint16_t intensity = kMaxIntensity) {
+    pinMode(pin_, OUTPUT);
+    // Use 50 kHz frequency to avoid interference with audio.
+    CHECK(ledcAttachChannel(pin_, 50000, 8, channel_));
+    CHECK(ledcWriteChannel(channel_, intensity));
+  }
+
+  void setIntensity(uint16_t intensity) override {
+    CHECK(ledcWriteChannel(channel_, intensity));
+  }
+
+ private:
+  uint8_t pin_;
+  int8_t channel_;
+};
+
 #else
+
+// With Arduino < 3, the channel must be specified explicitly.
+
+class LedcBacklitAtChannel : public Backlit {
+ public:
+  LedcBacklitAtChannel(uint8_t pin, uint8_t channel)
+      : pin_(pin), channel_(channel) {}
+
+ public:
+  void begin(uint16_t intensity = kMaxIntensity) {
+    pinMode(pin_, OUTPUT);
+    // Use 50 kHz frequency to avoid interference with audio.
     ledcSetup(channel_, 50000, 8);
     ledcAttachPin(pin_, channel_);
     ledcWrite(channel_, intensity);
-#endif
   }
 
- public:
-  void setIntensity(uint8_t intensity) override {
-#if (defined ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
-    ledcWrite(pin_, intensity);
-#else
+  void setIntensity(uint16_t intensity) override {
     ledcWrite(channel_, intensity);
-#endif
   }
 
  private:
-  int pin_;
-  int channel_;
+  uint8_t pin_;
+  uint8_t channel_;
 };
+
+#endif
 
 };  // namespace roo_display

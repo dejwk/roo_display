a81f68766e9dee935e5044b2711025630382293d
dejwk <dawid.kurzyniec@gmail.com>
2026-01-31 22:55:52 +0100

    Fixed SPI settings to match Arduino enum values.

--- src/roo_display/hal/spi_settings.h
+++ src/roo_display/hal/spi_settings.h
@@ -4,7 +4,7 @@ namespace roo_display {
 
 enum SpiDataMode { kSpiMode0 = 0, kSpiMode1 = 1, kSpiMode2 = 2, kSpiMode3 = 3 };
 
-enum SpiBitOrder { kSpiMsbFirst = 0, kSpiLsbFirst = 1 };
+enum SpiBitOrder { kSpiLsbFirst = 0, kSpiMsbFirst = 1 };
 
 template <uint32_t _clock, SpiBitOrder _bit_order, SpiDataMode _data_mode>
 struct SpiSettings {

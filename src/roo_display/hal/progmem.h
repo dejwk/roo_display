32b25babe8c7824edf7ad949bc12e1268f5bd4fd
dejwk <dawid.kurzyniec@gmail.com>
2026-02-25 03:23:10 +0100

    Added compile guards around progmem to remove build warnings.

--- src/roo_display/hal/progmem.h
+++ src/roo_display/hal/progmem.h
@@ -6,26 +6,43 @@
 #include <pgmspace.h>
 #endif
 #else  // !ARDUINO
+#ifndef PROGMEM
 #define PROGMEM
+#endif
+
+#ifndef pgm_read_byte
 #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#endif
+
+#ifndef pgm_read_word
 #define pgm_read_word(addr)           \
   ({                                  \
     typeof(addr) _addr = (addr);      \
     *(const unsigned short *)(_addr); \
   })
+#endif
+
+#ifndef pgm_read_dword
 #define pgm_read_dword(addr)         \
   ({                                 \
     typeof(addr) _addr = (addr);     \
     *(const unsigned long *)(_addr); \
   })
+#endif
+
+#ifndef pgm_read_float
 #define pgm_read_float(addr)     \
   ({                             \
     typeof(addr) _addr = (addr); \
     *(const float *)(_addr);     \
   })
+#endif
+
+#ifndef pgm_read_ptr
 #define pgm_read_ptr(addr)       \
   ({                             \
     typeof(addr) _addr = (addr); \
     *(void *const *)(_addr);     \
   })
+#endif
 #endif

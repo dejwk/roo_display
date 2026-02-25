84f8a674dbac0634f0316bcd580a7673548523e9
dejwk <dawid.kurzyniec@gmail.com>
2026-02-25 22:33:51 +0100

    Stripping non-builtin fonts from the fonts.ino example.

--- examples/fonts/fonts.ino
+++ examples/fonts/fonts.ino
@@ -1,11 +1,3 @@
-// NOTE: this test uses almost 100 font files of different sizes, and produces
-// a large binary (over 3MB). To deploy it to stock ESP32, you need to use a
-// non-standard partition table, e.g. huge_app.csv:
-// https://github.com/espressif/arduino-esp32/blob/master/tools/partitions/huge_app.csv
-
-// See
-// https://docs.platformio.org/en/latest/platforms/espressif32.html#partition-tables
-// for details.
 
 #include "Arduino.h"
 
@@ -43,104 +35,42 @@ struct Emulator {
 #include "roo_display/font/font.h"
 #include "roo_display/shape/basic.h"
 #include "roo_display/ui/text_label.h"
-#include "roo_fonts/NotoSansMono_Bold/12.h"
-#include "roo_fonts/NotoSansMono_Bold/18.h"
-#include "roo_fonts/NotoSansMono_Bold/27.h"
-#include "roo_fonts/NotoSansMono_Bold/40.h"
-#include "roo_fonts/NotoSansMono_Bold/60.h"
-#include "roo_fonts/NotoSansMono_Bold/8.h"
-#include "roo_fonts/NotoSansMono_Bold/90.h"
+#include "roo_fonts/NotoSansMono_Regular/10.h"
 #include "roo_fonts/NotoSansMono_Regular/12.h"
+#include "roo_fonts/NotoSansMono_Regular/15.h"
 #include "roo_fonts/NotoSansMono_Regular/18.h"
 #include "roo_fonts/NotoSansMono_Regular/27.h"
 #include "roo_fonts/NotoSansMono_Regular/40.h"
 #include "roo_fonts/NotoSansMono_Regular/60.h"
 #include "roo_fonts/NotoSansMono_Regular/8.h"
 #include "roo_fonts/NotoSansMono_Regular/90.h"
+#include "roo_fonts/NotoSans_Bold/10.h"
 #include "roo_fonts/NotoSans_Bold/12.h"
+#include "roo_fonts/NotoSans_Bold/15.h"
 #include "roo_fonts/NotoSans_Bold/18.h"
 #include "roo_fonts/NotoSans_Bold/27.h"
 #include "roo_fonts/NotoSans_Bold/40.h"
 #include "roo_fonts/NotoSans_Bold/60.h"
 #include "roo_fonts/NotoSans_Bold/8.h"
 #include "roo_fonts/NotoSans_Bold/90.h"
-#include "roo_fonts/NotoSans_BoldItalic/12.h"
-#include "roo_fonts/NotoSans_BoldItalic/18.h"
-#include "roo_fonts/NotoSans_BoldItalic/27.h"
-#include "roo_fonts/NotoSans_BoldItalic/40.h"
-#include "roo_fonts/NotoSans_BoldItalic/60.h"
-#include "roo_fonts/NotoSans_BoldItalic/8.h"
-#include "roo_fonts/NotoSans_BoldItalic/90.h"
-#include "roo_fonts/NotoSans_Condensed/12.h"
-#include "roo_fonts/NotoSans_Condensed/18.h"
-#include "roo_fonts/NotoSans_Condensed/27.h"
-#include "roo_fonts/NotoSans_Condensed/40.h"
-#include "roo_fonts/NotoSans_Condensed/60.h"
-#include "roo_fonts/NotoSans_Condensed/8.h"
-#include "roo_fonts/NotoSans_Condensed/90.h"
-#include "roo_fonts/NotoSans_CondensedItalic/12.h"
-#include "roo_fonts/NotoSans_CondensedItalic/18.h"
-#include "roo_fonts/NotoSans_CondensedItalic/27.h"
-#include "roo_fonts/NotoSans_CondensedItalic/40.h"
-#include "roo_fonts/NotoSans_CondensedItalic/60.h"
-#include "roo_fonts/NotoSans_CondensedItalic/8.h"
-#include "roo_fonts/NotoSans_CondensedItalic/90.h"
-#include "roo_fonts/NotoSans_Italic/12.h"
-#include "roo_fonts/NotoSans_Italic/18.h"
-#include "roo_fonts/NotoSans_Italic/27.h"
-#include "roo_fonts/NotoSans_Italic/40.h"
-#include "roo_fonts/NotoSans_Italic/60.h"
-#include "roo_fonts/NotoSans_Italic/8.h"
-#include "roo_fonts/NotoSans_Italic/90.h"
+#include "roo_fonts/NotoSans_Regular/10.h"
 #include "roo_fonts/NotoSans_Regular/12.h"
+#include "roo_fonts/NotoSans_Regular/15.h"
 #include "roo_fonts/NotoSans_Regular/18.h"
 #include "roo_fonts/NotoSans_Regular/27.h"
 #include "roo_fonts/NotoSans_Regular/40.h"
 #include "roo_fonts/NotoSans_Regular/60.h"
 #include "roo_fonts/NotoSans_Regular/8.h"
 #include "roo_fonts/NotoSans_Regular/90.h"
-#include "roo_fonts/NotoSerif_Bold/12.h"
-#include "roo_fonts/NotoSerif_Bold/18.h"
-#include "roo_fonts/NotoSerif_Bold/27.h"
-#include "roo_fonts/NotoSerif_Bold/40.h"
-#include "roo_fonts/NotoSerif_Bold/60.h"
-#include "roo_fonts/NotoSerif_Bold/8.h"
-#include "roo_fonts/NotoSerif_Bold/90.h"
-#include "roo_fonts/NotoSerif_BoldItalic/12.h"
-#include "roo_fonts/NotoSerif_BoldItalic/18.h"
-#include "roo_fonts/NotoSerif_BoldItalic/27.h"
-#include "roo_fonts/NotoSerif_BoldItalic/40.h"
-#include "roo_fonts/NotoSerif_BoldItalic/60.h"
-#include "roo_fonts/NotoSerif_BoldItalic/8.h"
-#include "roo_fonts/NotoSerif_BoldItalic/90.h"
-#include "roo_fonts/NotoSerif_Condensed/12.h"
-#include "roo_fonts/NotoSerif_Condensed/18.h"
-#include "roo_fonts/NotoSerif_Condensed/27.h"
-#include "roo_fonts/NotoSerif_Condensed/40.h"
-#include "roo_fonts/NotoSerif_Condensed/60.h"
-#include "roo_fonts/NotoSerif_Condensed/8.h"
-#include "roo_fonts/NotoSerif_Condensed/90.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/12.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/18.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/27.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/40.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/60.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/8.h"
-#include "roo_fonts/NotoSerif_CondensedItalic/90.h"
+#include "roo_fonts/NotoSerif_Italic/10.h"
 #include "roo_fonts/NotoSerif_Italic/12.h"
+#include "roo_fonts/NotoSerif_Italic/15.h"
 #include "roo_fonts/NotoSerif_Italic/18.h"
 #include "roo_fonts/NotoSerif_Italic/27.h"
 #include "roo_fonts/NotoSerif_Italic/40.h"
 #include "roo_fonts/NotoSerif_Italic/60.h"
 #include "roo_fonts/NotoSerif_Italic/8.h"
 #include "roo_fonts/NotoSerif_Italic/90.h"
-#include "roo_fonts/NotoSerif_Regular/12.h"
-#include "roo_fonts/NotoSerif_Regular/18.h"
-#include "roo_fonts/NotoSerif_Regular/27.h"
-#include "roo_fonts/NotoSerif_Regular/40.h"
-#include "roo_fonts/NotoSerif_Regular/60.h"
-#include "roo_fonts/NotoSerif_Regular/8.h"
-#include "roo_fonts/NotoSerif_Regular/90.h"
 
 using namespace roo_display;
 
@@ -151,12 +81,23 @@ St7789spi_240x240<5, 2, 4> device;
 Display display(device);
 
 struct FontFamily {
-  FontFamily(const Font& f8, const Font& f12, const Font& f18, const Font& f27,
-             const Font& f40, const Font& f60, const Font& f90)
-      : f8(f8), f12(f12), f18(f18), f27(f27), f40(f40), f60(f60), f90(f90) {}
+  FontFamily(const Font& f8, const Font& f10, const Font& f12, const Font& f15,
+             const Font& f18, const Font& f27, const Font& f40, const Font& f60,
+             const Font& f90)
+      : f8(f8),
+        f10(f10),
+        f12(f12),
+        f15(f15),
+        f18(f18),
+        f27(f27),
+        f40(f40),
+        f60(f60),
+        f90(f90) {}
 
   const Font& f8;
+  const Font& f10;
   const Font& f12;
+  const Font& f15;
   const Font& f18;
   const Font& f27;
   const Font& f40;
@@ -164,108 +105,32 @@ struct FontFamily {
   const Font& f90;
 };
 
-FontFamily notoSansRegular(font_NotoSans_Regular_8(),
-                           font_NotoSans_Regular_12(),
-                           font_NotoSans_Regular_18(),
-                           font_NotoSans_Regular_27(),
-                           font_NotoSans_Regular_40(),
-                           font_NotoSans_Regular_60(),
-                           font_NotoSans_Regular_90());
+FontFamily notoSansRegular(
+    font_NotoSans_Regular_8(), font_NotoSans_Regular_10(),
+    font_NotoSans_Regular_12(), font_NotoSans_Regular_15(),
+    font_NotoSans_Regular_18(), font_NotoSans_Regular_27(),
+    font_NotoSans_Regular_40(), font_NotoSans_Regular_60(),
+    font_NotoSans_Regular_90());
 
-FontFamily notoSansItalic(font_NotoSans_Italic_8(), font_NotoSans_Italic_12(),
-                          font_NotoSans_Italic_18(), font_NotoSans_Italic_27(),
-                          font_NotoSans_Italic_40(), font_NotoSans_Italic_60(),
-                          font_NotoSans_Italic_90());
-
-FontFamily notoSansBold(font_NotoSans_Bold_8(), font_NotoSans_Bold_12(),
+FontFamily notoSansBold(font_NotoSans_Bold_8(), font_NotoSans_Bold_10(),
+                        font_NotoSans_Bold_12(), font_NotoSans_Bold_15(),
                         font_NotoSans_Bold_18(), font_NotoSans_Bold_27(),
                         font_NotoSans_Bold_40(), font_NotoSans_Bold_60(),
                         font_NotoSans_Bold_90());
 
-FontFamily notoSansBoldItalic(font_NotoSans_BoldItalic_8(),
-                              font_NotoSans_BoldItalic_12(),
-                              font_NotoSans_BoldItalic_18(),
-                              font_NotoSans_BoldItalic_27(),
-                              font_NotoSans_BoldItalic_40(),
-                              font_NotoSans_BoldItalic_60(),
-                              font_NotoSans_BoldItalic_90());
+FontFamily notoSerifItalic(
+    font_NotoSerif_Italic_8(), font_NotoSerif_Italic_10(),
+    font_NotoSerif_Italic_12(), font_NotoSerif_Italic_15(),
+    font_NotoSerif_Italic_18(), font_NotoSerif_Italic_27(),
+    font_NotoSerif_Italic_40(), font_NotoSerif_Italic_60(),
+    font_NotoSerif_Italic_90());
 
-FontFamily notoSansCondensed(font_NotoSans_Condensed_8(),
-                             font_NotoSans_Condensed_12(),
-                             font_NotoSans_Condensed_18(),
-                             font_NotoSans_Condensed_27(),
-                             font_NotoSans_Condensed_40(),
-                             font_NotoSans_Condensed_60(),
-                             font_NotoSans_Condensed_90());
-
-FontFamily notoSansCondensedItalic(font_NotoSans_CondensedItalic_8(),
-                                   font_NotoSans_CondensedItalic_12(),
-                                   font_NotoSans_CondensedItalic_18(),
-                                   font_NotoSans_CondensedItalic_27(),
-                                   font_NotoSans_CondensedItalic_40(),
-                                   font_NotoSans_CondensedItalic_60(),
-                                   font_NotoSans_CondensedItalic_90());
-
-FontFamily notoSerifRegular(font_NotoSerif_Regular_8(),
-                            font_NotoSerif_Regular_12(),
-                            font_NotoSerif_Regular_18(),
-                            font_NotoSerif_Regular_27(),
-                            font_NotoSerif_Regular_40(),
-                            font_NotoSerif_Regular_60(),
-                            font_NotoSerif_Regular_90());
-
-FontFamily notoSerifItalic(font_NotoSerif_Italic_8(),
-                           font_NotoSerif_Italic_12(),
-                           font_NotoSerif_Italic_18(),
-                           font_NotoSerif_Italic_27(),
-                           font_NotoSerif_Italic_40(),
-                           font_NotoSerif_Italic_60(),
-                           font_NotoSerif_Italic_90());
-
-FontFamily notoSerifBold(font_NotoSerif_Bold_8(), font_NotoSerif_Bold_12(),
-                         font_NotoSerif_Bold_18(), font_NotoSerif_Bold_27(),
-                         font_NotoSerif_Bold_40(), font_NotoSerif_Bold_60(),
-                         font_NotoSerif_Bold_90());
-
-FontFamily notoSerifBoldItalic(font_NotoSerif_BoldItalic_8(),
-                               font_NotoSerif_BoldItalic_12(),
-                               font_NotoSerif_BoldItalic_18(),
-                               font_NotoSerif_BoldItalic_27(),
-                               font_NotoSerif_BoldItalic_40(),
-                               font_NotoSerif_BoldItalic_60(),
-                               font_NotoSerif_BoldItalic_90());
-
-FontFamily notoSerifCondensed(font_NotoSerif_Condensed_8(),
-                              font_NotoSerif_Condensed_12(),
-                              font_NotoSerif_Condensed_18(),
-                              font_NotoSerif_Condensed_27(),
-                              font_NotoSerif_Condensed_40(),
-                              font_NotoSerif_Condensed_60(),
-                              font_NotoSerif_Condensed_90());
-
-FontFamily notoSerifCondensedItalic(font_NotoSerif_CondensedItalic_8(),
-                                    font_NotoSerif_CondensedItalic_12(),
-                                    font_NotoSerif_CondensedItalic_18(),
-                                    font_NotoSerif_CondensedItalic_27(),
-                                    font_NotoSerif_CondensedItalic_40(),
-                                    font_NotoSerif_CondensedItalic_60(),
-                                    font_NotoSerif_CondensedItalic_90());
-
-FontFamily notoSansMonoRegular(font_NotoSansMono_Regular_8(),
-                               font_NotoSansMono_Regular_12(),
-                               font_NotoSansMono_Regular_18(),
-                               font_NotoSansMono_Regular_27(),
-                               font_NotoSansMono_Regular_40(),
-                               font_NotoSansMono_Regular_60(),
-                               font_NotoSansMono_Regular_90());
-
-FontFamily notoSansMonoBold(font_NotoSansMono_Bold_8(),
-                            font_NotoSansMono_Bold_12(),
-                            font_NotoSansMono_Bold_18(),
-                            font_NotoSansMono_Bold_27(),
-                            font_NotoSansMono_Bold_40(),
-                            font_NotoSansMono_Bold_60(),
-                            font_NotoSansMono_Bold_90());
+FontFamily notoSansMonoRegular(
+    font_NotoSansMono_Regular_8(), font_NotoSansMono_Regular_10(),
+    font_NotoSansMono_Regular_12(), font_NotoSansMono_Regular_15(),
+    font_NotoSansMono_Regular_18(), font_NotoSansMono_Regular_27(),
+    font_NotoSansMono_Regular_40(), font_NotoSansMono_Regular_60(),
+    font_NotoSansMono_Regular_90());
 
 void setup() {
   Serial.begin(9600);
@@ -288,8 +153,12 @@ void printText(const FontFamily& fonts) {
   Color color = color::Black;
   dc.setBackgroundColor(color::BlanchedAlmond);
   y += printLn(dc, fonts.f8, 10, y, text, color);
+  dc.setBackgroundColor(color::OldLace);
+  y += printLn(dc, fonts.f10, 10, y, text, color);
   dc.setBackgroundColor(color::LemonChiffon);
   y += printLn(dc, fonts.f12, 10, y, text, color);
+  dc.setBackgroundColor(color::Bisque);
+  y += printLn(dc, fonts.f15, 10, y, text, color);
   dc.setBackgroundColor(color::Beige);
   y += printLn(dc, fonts.f18, 10, y, text, color);
   dc.setBackgroundColor(color::PapayaWhip);
@@ -306,19 +175,9 @@ void printText(const FontFamily& fonts) {
 void loop() {
   // Sans serif fonts.
   printText(notoSansRegular);
-  printText(notoSansItalic);
   printText(notoSansBold);
-  printText(notoSansBoldItalic);
-  printText(notoSansCondensed);
-  printText(notoSansCondensedItalic);
   // Serif fonts.
-  printText(notoSerifRegular);
   printText(notoSerifItalic);
-  printText(notoSerifBold);
-  printText(notoSerifBoldItalic);
-  printText(notoSerifCondensed);
-  printText(notoSerifCondensedItalic);
   // Monotype font.
   printText(notoSansMonoRegular);
-  printText(notoSansMonoBold);
 }

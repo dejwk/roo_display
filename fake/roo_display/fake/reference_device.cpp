e34cdd773c9b4311e77a3ac1cced601635d5cfe9
dejwk <dawid.kurzyniec@gmail.com>
2026-03-09 01:39:07 +0100

    Revamped the fake reference device: * support a few color modes, * properly support orientation, * support emulation of 'blending' (framebuffer) devices (e.g. ESP32S3 displays), * more optimized implementation.

--- fake/roo_display/fake/reference_device.cpp
+++ fake/roo_display/fake/reference_device.cpp
@@ -1,117 +1,319 @@
 #include "fake/roo_display/fake/reference_device.h"
 
+#include <algorithm>
+
 #include "roo_display/color/blending.h"
 #include "roo_display/color/color.h"
 #include "roo_time.h"
 
 namespace roo_display {
 
-void ReferenceDisplayDevice::fillRect(int16_t x0, int16_t y0, int16_t x1,
-                                      int16_t y1, Color color) {
-  if (orientation().isXYswapped()) {
-    std::swap(x0, y0);
-    std::swap(x1, y1);
-  }
-  if (orientation().isBottomToTop()) {
-    std::swap(y0, y1);
-    y0 = raw_height() - y0 - 1;
-    y1 = raw_height() - y1 - 1;
-  }
-  if (orientation().isRightToLeft()) {
-    std::swap(x0, x1);
-    x0 = raw_width() - x0 - 1;
-    x1 = raw_width() - x1 - 1;
-  }
-  viewport_.fillRect(x0, y0, x1, y1, color.asArgb());
+template <typename Mode>
+void ReferenceDisplayDevice::initBackbuffer(const Mode& mode, Color initial) {
+  size_t bytes =
+      (static_cast<size_t>(Mode::bits_per_pixel) * raw_width() * raw_height() +
+       7) /
+      8;
+  backbuffer_storage_ = std::make_unique<roo::byte[]>(bytes);
+  auto output = std::make_unique<OffscreenDevice<Mode>>(
+      raw_width(), raw_height(), backbuffer_storage_.get(), mode);
+  output->fillRect(0, 0, raw_width() - 1, raw_height() - 1, initial);
+  auto raster = std::make_unique<ConstDramRasterBE<Mode>>(
+      raw_width(), raw_height(), backbuffer_storage_.get(), mode);
+  rasterizable_ = std::move(raster);
+  output_device_ = std::move(output);
 }
 
 ReferenceDisplayDevice::ReferenceDisplayDevice(
-    int w, int h, roo_testing_transducers::Viewport &viewport)
-    : DisplayDevice(w, h), viewport_(viewport), bgcolor_(0xFF7F7F7F) {
-  // viewport_.init(w, h);
-}
+    int w, int h, roo_testing_transducers::Viewport& viewport)
+    : ReferenceDisplayDevice(w, h, ColorMode::kRgb565, false, viewport) {}
 
-void ReferenceDisplayDevice::begin() {}
-void ReferenceDisplayDevice::end() { viewport_.flush(); }
-
-Color ReferenceDisplayDevice::effective_color(roo_display::BlendingMode mode,
-                                              Color color, Color bgcolor) {
-  return color;  // roo_display::ApplyBlending(mode, bgcolor, color);
-}
-
-void ReferenceDisplayDevice::write(roo_display::Color *colors,
-                                   uint32_t pixel_count) {
-  for (uint32_t i = 0; i < pixel_count; ++i) {
-    fillRect(cursor_x_, cursor_y_, cursor_x_, cursor_y_,
-             effective_color(blending_mode_, colors[i], bgcolor_));
-    advance();
+ReferenceDisplayDevice::ReferenceDisplayDevice(
+    int w, int h, ColorMode color_mode, bool blendable,
+    roo_testing_transducers::Viewport& viewport)
+    : DisplayDevice(w, h),
+      viewport_(viewport),
+      bgcolor_(0xFF7F7F7F),
+      addr_x0_(0),
+      addr_y0_(0),
+      addr_x1_(w - 1),
+      addr_y1_(h - 1),
+      blending_mode_(BlendingMode::kSourceOver),
+      color_mode_(color_mode),
+      blendable_(blendable),
+      orienter_(w, h) {
+  Color initial(0xFF, 0xF0, 0x80, 0x80);
+  switch (color_mode_) {
+    case ColorMode::kRgb888: {
+      initBackbuffer(Rgb888(), initial);
+      break;
+    }
+    case ColorMode::kRgb565: {
+      initBackbuffer(Rgb565(), initial);
+      break;
+    }
+    case ColorMode::kGrayscale8: {
+      initBackbuffer(Grayscale8(), initial);
+      break;
+    }
+    case ColorMode::kGrayscale1: {
+      initBackbuffer(Monochrome(Color(0xFF, 0xFF, 0xFF), Color(0, 0, 0)),
+                     initial);
+      break;
+    }
   }
 }
 
-// void ReferenceDevice::fill(PaintMode mode, roo_display::Color color,
-//                           uint32_t pixel_count) {
-//   for (uint32_t i = 0; i < pixel_count; ++i) {
-//     rectFill(cursor_x_, cursor_y_, 1, 1, effective_color(mode, color));
-//     advance();
-//   }
-// }
+void ReferenceDisplayDevice::begin() { output_device_->begin(); }
 
-void ReferenceDisplayDevice::writeRects(BlendingMode mode, Color *color,
-                                        int16_t *x0, int16_t *y0, int16_t *x1,
-                                        int16_t *y1, uint16_t count) {
-  while (count-- > 0) {
-    fillRect(*x0++, *y0++, *x1++, *y1++,
-             effective_color(mode, *color++, bgcolor_));
+void ReferenceDisplayDevice::end() {
+  output_device_->end();
+  viewport_.flush();
+}
+
+void ReferenceDisplayDevice::setAddress(uint16_t x0, uint16_t y0, uint16_t x1,
+                                        uint16_t y1,
+                                        roo_display::BlendingMode mode) {
+  addr_x0_ = x0;
+  addr_y0_ = y0;
+  addr_x1_ = x1;
+  addr_y1_ = y1;
+  cursor_x_ = x0;
+  cursor_y_ = y0;
+  blending_mode_ = mode;
+  output_device_->setAddress(x0, y0, x1, y1,
+                             blendable_ ? mode : BlendingMode::kSource);
+}
+
+const DisplayOutput::ColorFormat& ReferenceDisplayDevice::getColorFormat()
+    const {
+  return output_device_->getColorFormat();
+}
+
+void ReferenceDisplayDevice::orientationUpdated() {
+  output_device_->setOrientation(orientation());
+  orienter_.setOrientation(orientation());
+}
+
+void ReferenceDisplayDevice::blitRect(int16_t x0, int16_t y0, int16_t x1,
+                                      int16_t y1) {
+  orienter_.orientRect(x0, y0, x1, y1);
+  if (x0 > x1 || y0 > y1) return;
+
+  DCHECK_GE(x0, 0);
+  DCHECK_LT(x1, raw_width());
+  DCHECK_GE(y0, 0);
+  DCHECK_LT(y1, raw_height());
+
+  if (x0 == x1 && y0 == y1) {
+    Color pixel;
+    rasterizable_->readColorRect(x0, y0, x1, y1, &pixel);
+    viewport_.fillRect(x0, y0, x1, y1, pixel.asArgb());
+    return;
+  }
+
+  int32_t width = x1 - x0 + 1;
+  int32_t height = y1 - y0 + 1;
+  uint32_t area = width * height;
+  if (area == 0) return;
+
+  readback_color_tmp_.resize(area);
+  uint32_t* viewport_buffer = new uint32_t[area];
+  bool uniform =
+      rasterizable_->readColorRect(x0, y0, x1, y1, readback_color_tmp_.data());
+  if (uniform) {
+    uint32_t argb = readback_color_tmp_[0].asArgb();
+    for (uint32_t i = 0; i < area; ++i) {
+      viewport_buffer[i] = argb;
+    }
+  } else {
+    for (uint32_t i = 0; i < area; ++i) {
+      viewport_buffer[i] = readback_color_tmp_[i].asArgb();
+    }
+  }
+
+  viewport_.drawRect(x0, y0, x1, y1, viewport_buffer);
+}
+
+Color ReferenceDisplayDevice::preblend(roo_display::BlendingMode mode,
+                                       Color color) const {
+  return ApplyBlending(mode, bgcolor_, color);
+}
+
+void ReferenceDisplayDevice::fillRect(int16_t x0, int16_t y0, int16_t x1,
+                                      int16_t y1, BlendingMode mode,
+                                      Color color) {
+  if (!blendable_) {
+    color = preblend(mode, color);
+    mode = BlendingMode::kSource;
+  }
+  int16_t x0_copy = x0, y0_copy = y0, x1_copy = x1, y1_copy = y1;
+  output_device_->fillRects(mode, color, &x0, &y0, &x1, &y1, 1);
+  blitRect(x0_copy, y0_copy, x1_copy, y1_copy);
+}
+
+void ReferenceDisplayDevice::write(roo_display::Color* colors,
+                                   uint32_t pixel_count) {
+  if (pixel_count == 0) return;
+
+  int16_t start_x = cursor_x_;
+  int16_t start_y = cursor_y_;
+
+  output_device_->write(colors, pixel_count);
+  advance(pixel_count);
+  blitAdvancedRegion(start_x, start_y, cursor_x_, cursor_y_);
+}
+
+void ReferenceDisplayDevice::fill(roo_display::Color color,
+                                  uint32_t pixel_count) {
+  if (pixel_count == 0) return;
+
+  int16_t start_x = cursor_x_;
+  int16_t start_y = cursor_y_;
+
+  output_device_->fill(color, pixel_count);
+  advance(pixel_count);
+  blitAdvancedRegion(start_x, start_y, cursor_x_, cursor_y_);
+}
+
+void ReferenceDisplayDevice::advance(uint32_t pixel_count) {
+  if (pixel_count == 0) return;
+  if (addr_x1_ - cursor_x_ + 1 > pixel_count) {
+    // Fast path - still within the current line.
+    cursor_x_ += static_cast<int16_t>(pixel_count);
+    return;
+  }
+  pixel_count -= static_cast<uint32_t>(addr_x1_ - cursor_x_ + 1);
+  uint32_t window_width = static_cast<uint32_t>(addr_x1_ - addr_x0_ + 1);
+  cursor_y_ += static_cast<int16_t>(pixel_count / window_width) + 1;
+  cursor_x_ = addr_x0_ + static_cast<int16_t>(pixel_count % window_width);
+}
+
+void ReferenceDisplayDevice::blitAdvancedRegion(int16_t start_x,
+                                                int16_t start_y, int16_t end_x,
+                                                int16_t end_y) {
+  if (start_x == end_x && start_y == end_y) return;
+
+  // Defensive.
+  if (end_y < start_y || (end_y == start_y && end_x <= start_x)) {
+    blitRect(addr_x0_, addr_y0_, addr_x1_, addr_y1_);
+    return;
+  }
+
+  // Single line - quick path.
+  if (start_y == end_y) {
+    blitRect(start_x, start_y, end_x - 1, end_y);
+    return;
+  }
+
+  // Finish the first line.
+  blitRect(start_x, start_y, addr_x1_, start_y);
+
+  // Fill the middle lines.
+  if (start_y + 1 <= end_y - 1) {
+    blitRect(addr_x0_, start_y + 1, addr_x1_, end_y - 1);
+  }
+
+  // Draw the last line if needed.
+  if (end_x > addr_x0_) {
+    blitRect(addr_x0_, end_y, end_x - 1, end_y);
+  }
+}
+
+void ReferenceDisplayDevice::writeRects(BlendingMode mode, Color* color,
+                                        int16_t* x0, int16_t* y0, int16_t* x1,
+                                        int16_t* y1, uint16_t count) {
+  if (count == 0) return;
+  for (uint16_t i = 0; i < count; ++i) {
+    fillRect(x0[i], y0[i], x1[i], y1[i], mode, color[i]);
   }
 }
 
 void ReferenceDisplayDevice::fillRects(BlendingMode mode, Color color,
-                                       int16_t *x0, int16_t *y0, int16_t *x1,
-                                       int16_t *y1, uint16_t count) {
-  while (count-- > 0) {
-    fillRect(*x0++, *y0++, *x1++, *y1++,
-             effective_color(mode, color, bgcolor_));
+                                       int16_t* x0, int16_t* y0, int16_t* x1,
+                                       int16_t* y1, uint16_t count) {
+  if (count == 0) return;
+  for (uint16_t i = 0; i < count; ++i) {
+    fillRect(x0[i], y0[i], x1[i], y1[i], mode, color);
   }
 }
 
 void ReferenceDisplayDevice::writePixels(BlendingMode mode,
-                                         roo_display::Color *colors,
-                                         int16_t *xs, int16_t *ys,
+                                         roo_display::Color* colors,
+                                         int16_t* xs, int16_t* ys,
                                          uint16_t pixel_count) {
-  for (int i = 0; i < pixel_count; ++i) {
-    fillRect(xs[i], ys[i], xs[i], ys[i],
-             effective_color(mode, colors[i], bgcolor_));
+  if (pixel_count == 0) return;
+
+  constexpr uint16_t kBlockSize = 64;
+  while (pixel_count > 0) {
+    uint16_t block_count = std::min<uint16_t>(kBlockSize, pixel_count);
+
+    int16_t xs_copy[kBlockSize];
+    int16_t ys_copy[kBlockSize];
+    std::copy(xs, xs + block_count, xs_copy);
+    std::copy(ys, ys + block_count, ys_copy);
+
+    output_device_->writePixels(mode, colors, xs, ys, block_count);
+    blitPixels(xs_copy, ys_copy, block_count);
+
+    pixel_count -= block_count;
+    colors += block_count;
+    xs += block_count;
+    ys += block_count;
   }
 }
 
 void ReferenceDisplayDevice::fillPixels(BlendingMode mode,
-                                        roo_display::Color color, int16_t *xs,
-                                        int16_t *ys, uint16_t pixel_count) {
-  for (int i = 0; i < pixel_count; ++i) {
-    fillRect(xs[i], ys[i], xs[i], ys[i],
-             effective_color(mode, color, bgcolor_));
+                                        roo_display::Color color, int16_t* xs,
+                                        int16_t* ys, uint16_t pixel_count) {
+  if (pixel_count == 0) return;
+
+  constexpr uint16_t kBlockSize = 64;
+  while (pixel_count > 0) {
+    uint16_t block_count = std::min<uint16_t>(kBlockSize, pixel_count);
+
+    int16_t xs_copy[kBlockSize];
+    int16_t ys_copy[kBlockSize];
+    std::copy(xs, xs + block_count, xs_copy);
+    std::copy(ys, ys + block_count, ys_copy);
+
+    output_device_->fillPixels(mode, color, xs, ys, block_count);
+    blitPixels(xs_copy, ys_copy, block_count);
+
+    pixel_count -= block_count;
+    xs += block_count;
+    ys += block_count;
   }
 }
 
-void ReferenceDisplayDevice::advance() {
-  ++cursor_x_;
-  if (cursor_x_ > addr_x1_) {
-    cursor_x_ = addr_x0_;
-    ++cursor_y_;
+void ReferenceDisplayDevice::blitPixels(int16_t* xs, int16_t* ys,
+                                        uint16_t pixel_count) {
+  if (pixel_count == 0) return;
+
+  int16_t* xs_oriented = xs;
+  int16_t* ys_oriented = ys;
+  orienter_.orientPixels(xs_oriented, ys_oriented, pixel_count);
+
+  constexpr uint16_t kBlockSize = 64;
+  Color colors[kBlockSize];
+  rasterizable_->readColors(xs_oriented, ys_oriented, pixel_count, colors);
+
+  for (uint16_t i = 0; i < pixel_count; ++i) {
+    viewport_.fillRect(xs_oriented[i], ys_oriented[i], xs_oriented[i],
+                       ys_oriented[i], colors[i].asArgb());
   }
 }
 
 void ReferenceDisplayDevice::init() {
   viewport_.init(raw_width(), raw_height());
-  viewport_.fillRect(0, 0, raw_width() - 1, raw_height() - 1, 0xFFF08080);
 }
 
 ReferenceTouchDevice::ReferenceTouchDevice(
-    roo_testing_transducers::Viewport &viewport)
+    roo_testing_transducers::Viewport& viewport)
     : TouchDevice(), viewport_(viewport) {}
 
 roo_display::TouchResult ReferenceTouchDevice::getTouch(
-    roo_display::TouchPoint *points, int max_points) {
+    roo_display::TouchPoint* points, int max_points) {
   int16_t w = viewport_.width();
   int16_t h = viewport_.height();
   if (w < 0 || h < 0) {
@@ -121,7 +323,7 @@ roo_display::TouchResult ReferenceTouchDevice::getTouch(
   int16_t x_display, y_display;
   bool result = viewport_.isMouseClicked(&x_display, &y_display);
   if (result) {
-    roo_display::TouchPoint &tp = points[0];
+    roo_display::TouchPoint& tp = points[0];
     tp.id = 1;
     tp.x = (int32_t)4096 * x_display / w;
     tp.y = (int32_t)4096 * y_display / h;
--- fake/roo_display/fake/reference_device.h
+++ fake/roo_display/fake/reference_device.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "roo_display/color/color_modes.h"
 #include "roo_display/core/device.h"
+#include "roo_display/core/offscreen.h"
+#include "roo_display/core/rasterizable.h"
 #include "roo_display/internal/color_format.h"
 #include "roo_display/internal/color_io.h"
 #include "roo_display/products/combo_device.h"
@@ -13,7 +16,12 @@ namespace roo_display {
 
 class ReferenceDisplayDevice : public DisplayDevice {
  public:
+  enum class ColorMode { kRgb888, kRgb565, kGrayscale8, kGrayscale1 };
+
   ReferenceDisplayDevice(int w, int h,
+                         roo_testing_transducers::Viewport& viewport);
+
+  ReferenceDisplayDevice(int w, int h, ColorMode color_mode, bool blendable,
                          roo_testing_transducers::Viewport& viewport);
 
   ~ReferenceDisplayDevice() {}
@@ -23,20 +31,11 @@ class ReferenceDisplayDevice : public DisplayDevice {
   void end() override;
 
   void setAddress(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
-                  roo_display::BlendingMode blending_mode) override {
-    addr_x0_ = x0;
-    addr_y0_ = y0;
-    addr_x1_ = x1;
-    addr_y1_ = y1;
-    blending_mode_ = blending_mode;
-    cursor_x_ = addr_x0_;
-    cursor_y_ = addr_y0_;
-  }
+                  roo_display::BlendingMode blending_mode) override;
 
   void write(roo_display::Color* colors, uint32_t pixel_count) override;
 
-  //  void fill(roo_display::PaintMode mode, roo_display::Color color, uint32_t
-  //  pixel_count) override;
+  void fill(roo_display::Color color, uint32_t pixel_count) override;
 
   void writeRects(roo_display::BlendingMode mode, roo_display::Color* color,
                   int16_t* x0, int16_t* y0, int16_t* x1, int16_t* y1,
@@ -56,28 +55,42 @@ class ReferenceDisplayDevice : public DisplayDevice {
     bgcolor_ = bgcolor;
   }
 
-  const ColorFormat& getColorFormat() const override {
-    static const Argb8888 mode;
-    static const internal::ColorFormatImpl<Argb8888, roo_io::kBigEndian>
-        color_format(mode);
-    return color_format;
-  }
+  const ColorFormat& getColorFormat() const override;
+
+  void orientationUpdated() override;
 
  private:
-  static roo_display::Color effective_color(roo_display::BlendingMode mode,
-                                            roo_display::Color color,
-                                            roo_display::Color bgcolor);
+  template <typename Mode>
+  void initBackbuffer(const Mode& mode, roo_display::Color initial);
+
+  void blitRect(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+  roo_display::Color preblend(roo_display::BlendingMode mode,
+                              roo_display::Color color) const;
 
   void fillRect(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-                roo_display::Color color);
+                roo_display::BlendingMode mode, roo_display::Color color);
 
-  void advance();
+  void blitPixels(int16_t* xs, int16_t* ys, uint16_t pixel_count);
+
+  void blitAdvancedRegion(int16_t start_x, int16_t start_y, int16_t end_x,
+                          int16_t end_y);
+
+  void advance(uint32_t pixel_count);
 
   roo_testing_transducers::Viewport& viewport_;
   roo_display::Color bgcolor_;
   int16_t addr_x0_, addr_y0_, addr_x1_, addr_y1_;
-  roo_display::BlendingMode blending_mode_;
   int16_t cursor_x_, cursor_y_;
+  roo_display::BlendingMode blending_mode_;
+  ColorMode color_mode_;
+  bool blendable_;
+  std::unique_ptr<roo::byte[]> backbuffer_storage_;
+  std::unique_ptr<Rasterizable> rasterizable_;
+  std::unique_ptr<DisplayDevice> output_device_;
+  internal::Orienter orienter_;
+  internal::AddressWindow address_window_;
+  std::vector<roo_display::Color> readback_color_tmp_;
 };
 
 class ReferenceTouchDevice : public roo_display::TouchDevice {
@@ -95,7 +108,14 @@ class ReferenceComboDevice : public roo_display::ComboDevice {
  public:
   ReferenceComboDevice(int w, int h,
                        roo_testing_transducers::Viewport& viewport)
-      : display_(w, h, viewport), touch_(viewport) {}
+      : ReferenceComboDevice(w, h, ReferenceDisplayDevice::ColorMode::kRgb565,
+                             false, viewport) {}
+
+  ReferenceComboDevice(int w, int h,
+                       ReferenceDisplayDevice::ColorMode color_mode,
+                       bool blendable,
+                       roo_testing_transducers::Viewport& viewport)
+      : display_(w, h, color_mode, blendable, viewport), touch_(viewport) {}
 
   roo_display::DisplayDevice& display() override { return display_; }
 

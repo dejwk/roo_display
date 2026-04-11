6ee2b1fef20260b87aa7b7f5edcdc52dd43ebe11
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2026-04-11 17:31:11 +0200

    Refactored async config.

--- src/roo_display/hal/esp32/spi_config.cpp
+++ src/roo_display/hal/esp32/spi_config.cpp
@@ -1,3 +1,10 @@
 #include "roo_display/hal/esp32/spi_config.h"
 
-ROO_FLAG(int, roo_display_esp32_spi_async, 3);
+using namespace roo_display::esp32;
+
+#ifndef ROO_DISPLAY_ESP32_DEFAULT_SPI_ASYNC_MODE
+#define ROO_DISPLAY_ESP32_DEFAULT_SPI_ASYNC_MODE 0
+#endif
+
+ROO_FLAG(SpiAsyncMode, roo_display_esp32_spi_async,
+         (SpiAsyncMode)(ROO_DISPLAY_ESP32_DEFAULT_SPI_ASYNC_MODE));
--- src/roo_display/hal/esp32/spi_config.h
+++ src/roo_display/hal/esp32/spi_config.h
@@ -2,27 +2,34 @@
 
 #include "roo_flags.h"
 
-// 0: async disabled (always synchronous).
-// 1: async enabled with eager completion on await.
-// 2: async enabled with no eager completion.
-// 3: DMA-backed async pipeline.
-ROO_DECLARE_FLAG(int, roo_display_esp32_spi_async);
-
 namespace roo_display {
 namespace esp32 {
 
 enum SpiAsyncMode : int {
-	kSpiAsyncModeSync = 0,
-	kSpiAsyncModeIsrEager = 1,
-	kSpiAsyncModeIsrStrict = 2,
-	kSpiAsyncModeDmaPipeline = 3,
+  // No-IRQ.
+  kSpiAsyncModeSync = 0,
+  // IRQ-based async with eager, busy-loop completion on await.
+  kSpiAsyncModeIsrEager = 1,
+  // IRQ-based async with no eager completion.
+  kSpiAsyncModeIsrStrict = 2,
+  // DMA-backed async pipeline.
+  kSpiAsyncModeDmaPipeline = 3,
 };
 
+}
+}  // namespace roo_display
+
+// 0: async disabled (always synchronous).
+// 1: async enabled with eager completion on await.
+// 2: async enabled with no eager completion.
+// 3: DMA-backed async pipeline.
+ROO_DECLARE_FLAG(roo_display::esp32::SpiAsyncMode, roo_display_esp32_spi_async);
+
+namespace roo_display {
+namespace esp32 {
+
 inline SpiAsyncMode GetSpiAsyncModeFromFlag() {
-	int mode = GET_ROO_FLAG(roo_display_esp32_spi_async);
-	if (mode <= 0) return kSpiAsyncModeSync;
-	if (mode >= 3) return kSpiAsyncModeDmaPipeline;
-	return static_cast<SpiAsyncMode>(mode);
+  return GET_ROO_FLAG(roo_display_esp32_spi_async);
 }
 
 }  // namespace esp32
